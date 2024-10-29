3ff53aceeefe4e65e05bb9866bce091ae7e9e5e8
Carlos Cano <carlosqhano002@gmail.com>
2024-10-29 12:05:22 -0600

    Changes Cano

--- resources/views/Landing/index.blade.php
+++ resources/views/Landing/index.blade.php
@@ -18,7 +18,7 @@
 
     </section>
 
-<div x-data="{ open: false }" class="w-full text-gray-700 bg-blue-300">
+<div x-data="{ open: false }" class="w-full text-white bg-gray-900">
     <div class="flex flex-col max-w-screen-xl px-8 mx-auto md:items-center md:justify-between md:flex-row">
         <div class="flex flex-row items-center justify-between py-6">
             <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
@@ -28,17 +28,19 @@
                 </svg>
             </button>
         </div>
-        <nav :class="{ 'transform md:transform-none': !open, 'h-full': open }" class="h-0 md:h-auto flex flex-col flex-grow md:items-center pb-4 md:pb-0 md:flex md:justify-end md:flex-row origin-top duration-300 scale-y-0">
-            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" <a href="{{ route('landing.index') }}">Home</a>
-            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" <a href="{{ route('landing.proyectos') }}">Proyectos</a>
-            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" <a href="{{ route('landing.blogs') }}">Blogs</a>
-            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#">About Us</a>
-            <a class="px-10 py-3 mt-2 text-sm text-center bg-white text-gray-800 rounded-full md:mt-8 md:ml-4" href="#">Login</a>
-            <a class="px-10 py-3 mt-2 text-sm text-center bg-yellow-500 text-white rounded-full md:mt-8 md:ml-4" href="#">Sign Up</a>
+        
+        <nav :class="{ 'transform md:transform-none': !open, 'h-full': open }" class="h-0 md:h-auto flex flex-col md:flex-row md:items-center pb-4 md:pb-0 md:justify-end origin-top duration-300 scale-y-0">
+            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="{{ route('landing.index') }}">Home</a>
+            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="{{ route('landing.proyectos') }}">Proyectos</a>
+            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="{{ route('landing.blogs') }}">Blogs</a>
+            <a class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#">About Us</a>
+            <a class="px-6 py-2 mt-2 text-sm text-center bg-white text-gray-800 rounded-full md:mt-0 md:ml-4 hover:bg-gray-200" href="#">Login</a>
+            <a class="px-6 py-2 mt-2 text-sm text-center bg-yellow-500 text-white rounded-full md:mt-0 md:ml-4 hover:bg-yellow-600" href="#">Sign Up</a>
         </nav>
+
     </div>
 </div>
-    <div class="bg-green-500">
+    <div class="bg-gray-100">
         <div class="max-w-screen-xl px-8 mx-auto flex flex-col lg:flex-row items-start">
             <div class="flex flex-col w-full lg:w-6/12 justify-center lg:pt-24 items-start text-center lg:text-left mb-5 md:mb-0">
                 <h1 data-aos="fade-right" data-aos-once="true" class="my-4 text-5xl font-bold leading-tight text-darken">
